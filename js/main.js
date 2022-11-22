085e29224a10d532df36ac62ad9a104a3a4500a4
Sulaiman Hussain <118259411+SulaimanROC@users.noreply.github.com>
2022-11-22 15:30:48 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//const hello = document.querySelector(".intro");
+//hello.textContent = "Dit is mijn eerste querySelector";
+
+//const yes = document.querySelector(".blog");
+//yes.textContent = "Hier schrijf ik mijn blog artikel";
+
+//const ok = document.querySelector(".mix");
+//ok.innerHTML = "<h1>Deze tekst bevat ook een H element</h1>";
+
+//const yup = document.querySelector(".todo");
+
+//for (let i = 1; i < 10; i++) {
+  //  yup.innerHTML += "<li><button>boodschappen</button></li>"
+//}
+
+//const okay = document.querySelector(".item");
+//const list = document.querySelectorAll('.item');
+
+//const llist = document.querySelectorAll('.item');
+//let i = 0;
+//while (i < llist.length) {
+ //   llist[i].textContent = 'list'     
+   // i++;
+//}
+
+//const color = document.querySelector(".styling");
+//color.innerHTML = "<p> Dit is een element met styling </p>";
+//color.style.background = "violet"
+
+const Suli = document.querySelector(".box");
+Suli.innerHTML = "<p> Dit is een element met border </p>";
+Suli.style.border = "1px solid black";
+
+const father = document.querySelector(".wrapper");
+father.innerHTML = '<div class=".card"></div>';
+//  father.innerHTML = '<img src="https://www.w3schools.com/howto/img_avatar.png'alt="Avatar" style="width:100%"'>
